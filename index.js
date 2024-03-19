8a92fb14aee12b3c7da436e3e3e6339d5b657574
Will Metzler <metzlerwill@gmail.com>
2024-03-18 20:58:14 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(books => {renderBooks(books)
+    return books;
+  });
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
